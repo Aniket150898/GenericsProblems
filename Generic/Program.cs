99c617df5ee790729d429ad639381b597361652d
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-01-17 11:44:39 +0530

    [Aniket]- Welcome message

---
+++ Generic/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Generic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Guy's!!!, Welcome To Generic Problems ");
+        }
+    }
+}
